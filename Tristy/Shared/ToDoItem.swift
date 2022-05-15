df6d24d3be2807ad261f226299f9e250d53fc127
fraander <57777918+fraander@users.noreply.github.com>
2022-05-15 17:10:41 -0400

    Initial code from SimpleToDo by Paul Hudson for basic sync/file structure
    https://github.com/twostraws/simple-swiftui/tree/main/SimpleToDo

---
+++ Tristy/Shared/ToDoItem.swift
@@ -0,0 +1,69 @@
+//
+// ContentView.swift
+// SimpleToDo
+//
+// Copyright © 2022 Paul Hudson.
+// Licensed under MIT license.
+//
+// https://github.com/twostraws/simple-swiftui
+// See LICENSE for license information
+//
+
+import Foundation
+
+/// One item in our to do list.
+struct ToDoItem: Codable, Identifiable, Hashable {
+    
+    static let example = ToDoItem(id: UUID(), title: "New todo item that has a long name", priority: .high, isComplete: false)
+    
+    /// The priority for a single item; uses `CaseIterable` so we loop over all three cases in `DetailView`.
+    enum Priority: String, Codable, CaseIterable {
+        case low = "Low"
+        case medium = "Medium"
+        case high = "High"
+    }
+
+    /// A unique, random identifier for this item.
+    var id = UUID()
+
+    /// The user-facing title of this item.
+    var title = "New Item"
+
+    /// The current priority for this item.
+    var priority = Priority.medium
+
+    /// Whether this item has been completed or not.
+    var isComplete = false
+
+    /// The SF Symbol icon name to use for this item.
+    var icon: String {
+        if isComplete {
+            return "checkmark.square"
+        } else {
+            switch priority {
+            case .low:
+                return "arrow.down.square"
+            case .medium:
+                return "square"
+            default:
+                return "exclamationmark.square"
+            }
+        }
+    }
+
+    /// The value of the item, to convey this information to assistive technologies.
+    var accessibilityValue: String {
+        if isComplete {
+            return "completed"
+        } else {
+            switch priority {
+            case .low:
+                return "low priority"
+            case .medium:
+                return "" // To prevent verbosity, don't set a specific value for the default priority.
+            case .high:
+                return "high priority"
+            }
+        }
+    }
+}
