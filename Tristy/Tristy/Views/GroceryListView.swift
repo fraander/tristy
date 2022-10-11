4ecb8e086d88dcdc3d8d113e2c78a74f15f09833
fraander <57777918+fraander@users.noreply.github.com>
2022-10-11 11:58:54 -0400

    Added toolbar and new grocery text field

--- Tristy/Tristy/Views/GroceryListView.swift
+++ Tristy/Tristy/Views/GroceryListView.swift
@@ -9,28 +9,78 @@ import SwiftUI
 
 struct GroceryListView: View {
     
+    enum Focus {
+        case addField, none
+    }
+    
+    @Environment(\.colorScheme) var colorMode
     @ObservedObject var groceryListVM = GroceryListViewModel()
+    @State var text = ""
+    @FocusState var focusState: Focus?
+    
+    var clearAllButton: some View {
+        Button(role: .destructive) {
+            groceryListVM.groceryVMs.forEach{$0.remove()}
+        } label: {
+            Label("Clear All", systemImage: "eraser.line.dashed")
+        }
+    }
+    
+    var listOfGrocery: some View {
+        List {
+            ForEach(groceryListVM.groceryVMs) { groceryVM in
+                GroceryView(groceryVM: groceryVM)
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+    
+    var addGroceryButton: some View {
+        HStack {
+            Button {
+                addGrocery(title: text)
+            } label: {
+                Image(systemName: "plus")
+            }
+            .tint(focusState == .addField ? Color.accentColor : Color.secondary)
+            
+            TextField("Add item...", text: $text)
+                .focused($focusState, equals: .addField)
+                .onSubmit {
+                    addGrocery(title: text)
+                }
+                .submitLabel(.done)
+            
+            // add ways to change attributes of the text
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 10.0)
+                .strokeBorder(Color.secondary, lineWidth: 1)
+                .background {
+                    RoundedRectangle(cornerRadius: 10).fill(Color(uiColor: .systemBackground))
+                }
+                .shadow(
+                    color: focusState == .addField ? Color.accentColor : Color.clear,
+                    radius: focusState == .addField ? 3 : 0
+                )
+                .animation(Animation.easeInOut(duration: 0.25), value: focusState)
+        }
+        .padding()
+    }
     
     var body: some View {
-        VStack {
-            Button(role: .destructive) {
-                groceryListVM.groceryVMs.forEach{$0.remove()}
-            } label: {
-                Label("Clear All", systemImage: "eraser.line.dashed")
+        NavigationStack {
+            ZStack {
+                listOfGrocery
+                
+                addGroceryButton
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             }
-            
-            
-            List {
-                ForEach(groceryListVM.groceryVMs) { groceryVM in
-                    GroceryView(groceryVM: groceryVM)
-                }
-                .onDelete(perform: deleteItems)
-            }
-            
-            Button {
-                addGrocery()
-            } label: {
-                Label("Add item", systemImage: "plus")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Groceries")
+            .toolbarTitleMenu {
+                clearAllButton
             }
         }
     }
@@ -39,9 +89,13 @@ struct GroceryListView: View {
         items.forEach { groceryListVM.groceryVMs[$0].remove() }
     }
     
-    private func addGrocery() {
-        let grocery = Grocery(title: "\(groceryListVM.groceryVMs.count)")
-        groceryListVM.add(grocery)
+    private func addGrocery(title: String) {
+        if (!text.isEmpty) {
+            let grocery = Grocery(title: title)
+            groceryListVM.add(grocery)
+            text = ""
+            focusState = GroceryListView.Focus.none
+        }
     }
 }
 
@@ -53,5 +107,9 @@ struct GroceryListView_Previews: PreviewProvider {
     
     static var previews: some View {
         GroceryListView(groceryListVM: GroceryListViewModel(listOfVM))
+            .previewDisplayName("Populated list")
+        
+        GroceryListView(groceryListVM: GroceryListViewModel([]))
+            .previewDisplayName("Empty list")
     }
 }
